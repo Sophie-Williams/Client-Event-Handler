292eafa3179eb416714e9c604d328d13c921803f
Muhammet <33325253+blackdragonx61@users.noreply.github.com>
2019-09-24 16:58:28 +0300

    Update PythonApplication.cpp

--- Svn/Client/UserInterface/PythonApplication.cpp
+++ Svn/Client/UserInterface/PythonApplication.cpp
@@ -4,9 +4,9 @@
 #endif
 
 //Find
-	s_uiLoad += ELTimer_GetMSec() - dwStart;
+		s_uiLoad = s_dwFaceCount = s_dwUpdateFrameCount = s_dwRenderFrameCount = 0;
 	
 ///Add
 #if defined(EVENT_HANDLER_MASTER)
-	EventHandler::Instance().Proccess();
+		EventHandler::Instance().Proccess();
 #endif
